04752b4fcd9f53bd9b9cbed7c7943eb905431819
Lihang Ying <lihang@industrialnext.ai>
2022-12-14 11:22:53 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="u2net",
+    version="0.1.0",
+    author="",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)

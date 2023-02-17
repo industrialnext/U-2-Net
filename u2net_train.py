98c6ce2f84c4432cce36f912288d1f2a79230610
Elton Shon <elton@industrialnext.ai>
2023-02-17 13:46:53 -0800

    add arguments

--- u2net_train.py
+++ u2net_train.py
@@ -24,6 +24,7 @@ from data_loader import SalObjDataset
 from model import U2NET
 from model import U2NETP
 
+import argparse
 
 # ------- 1. define loss function --------
 def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
@@ -46,16 +47,26 @@ def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
 # ------- 2. set the directory of training dataset --------
 model_name = 'u2net'  # 'u2netp'
 
-data_dir = "data/"
-tra_label_dir = "label/"
-tra_image_dir = "fender5/train/"
-
-image_ext = '.jpg'
-label_ext = '.png'
-
-model_dir = os.path.join(os.getcwd(), 'saved_models', model_name + os.sep)
+label_ext = ".png" # Label should all be in this format
+supported_ext = (".jpg", ".png")
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Train")
+    parser.add_argument("-i", required=True, action="store", help="Training image directory")
+    parser.add_argument("-l", required=True, action="store", help="Training label directory")
+    parser.add_argument("-m", required=True, action="store", help="Saved model directory name")
+
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.i) or not os.path.isdir(args.l):
+        print("Paths don't exist!")
+        exit()
+
+    if os.path.isabs(args.m):
+        model_dir = os.path.join(args.m, model_name + os.sep)
+    else:
+        model_dir = os.path.join(os.getcwd(), args.m, model_name + os.sep)
+
     if not os.path.exists(model_dir):
         print(f"mkdir {model_dir}")
         os.makedirs(model_dir)
@@ -66,8 +77,11 @@ if __name__ == '__main__':
     train_num = 0
     val_num = 0
     
-    tra_img_name_list = glob.glob(data_dir + tra_image_dir + '*' + image_ext)
-    
+    tra_img_name_list = []
+
+    for ext in supported_ext:
+        tra_img_name_list.extend(glob.glob(os.path.join(args.i, '*' + ext)))
+
     tra_lbl_name_list = []
     for img_path in tra_img_name_list:
         img_name = img_path.split(os.sep)[-1]
@@ -77,16 +91,16 @@ if __name__ == '__main__':
         imidx = bbb[0]
         for i in range(1, len(bbb)):
             imidx = imidx + "." + bbb[i]
-    
-        tra_lbl_name_list.append(data_dir + tra_label_dir + imidx + label_ext)
-    
+
+        tra_lbl_name_list.append(os.path.join(args.l, imidx + label_ext))
+
     print("---")
     print("train images: ", len(tra_img_name_list))
     print("train labels: ", len(tra_lbl_name_list))
     print("---")
-    
+
     train_num = len(tra_img_name_list)
-    
+
     salobj_dataset = SalObjDataset(
         img_name_list=tra_img_name_list,
         lbl_name_list=tra_lbl_name_list,
@@ -96,22 +110,22 @@ if __name__ == '__main__':
             ToTensorLab(flag=0)]))
     salobj_dataloader = DataLoader(
         salobj_dataset, batch_size=batch_size_train, shuffle=True, num_workers=1)
-    
+
     # ------- 3. define model --------
     # define the net
     if (model_name == 'u2net'):
         net = U2NET(3, 1)
     elif (model_name == 'u2netp'):
         net = U2NETP(3, 1)
-    
+
     if torch.cuda.is_available():
         net.cuda()
-    
+
     # ------- 4. define optimizer --------
     print("---define optimizer...")
     optimizer = optim.Adam(net.parameters(), lr=0.001, betas=(
         0.9, 0.999), eps=1e-08, weight_decay=0)
-    
+
     # ------- 5. training process --------
     print("---start training...")
     ite_num = 0
@@ -119,19 +133,19 @@ if __name__ == '__main__':
     running_tar_loss = 0.0
     ite_num4val = 0
     save_frq = 2000  # save the model every 2000 iterations
-    
+
     for epoch in range(0, epoch_num):
         net.train()
-    
+
         for i, data in enumerate(salobj_dataloader):
             ite_num = ite_num + 1
             ite_num4val = ite_num4val + 1
-    
+
             inputs, labels = data['image'], data['label']
-    
+
             inputs = inputs.type(torch.FloatTensor)
             labels = labels.type(torch.FloatTensor)
-    
+
             # wrap them in Variable
             if torch.cuda.is_available():
                 inputs_v, labels_v = Variable(inputs.cuda(), requires_grad=False), Variable(labels.cuda(),
@@ -139,30 +153,30 @@ if __name__ == '__main__':
             else:
                 inputs_v, labels_v = Variable(inputs, requires_grad=False), Variable(
                     labels, requires_grad=False)
-    
+
             # y zero the parameter gradients
             optimizer.zero_grad()
-    
+
             # forward + backward + optimize
             d0, d1, d2, d3, d4, d5, d6 = net(inputs_v)
             loss2, loss = muti_bce_loss_fusion(
                 d0, d1, d2, d3, d4, d5, d6, labels_v)
-    
+
             loss.backward()
             optimizer.step()
-    
+
             # # print statistics
             running_loss += loss.data.item()
             running_tar_loss += loss2.data.item()
-    
+
             # del temporary outputs and loss
             del d0, d1, d2, d3, d4, d5, d6, loss2, loss
-    
+
             print("[epoch: %3d/%3d, batch: %5d/%5d, ite: %d] train loss: %3f, tar: %3f " % (
                 epoch + 1, epoch_num, (i + 1) * batch_size_train, train_num, ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
-    
+
             if ite_num % save_frq == 0:
-    
+
                 torch.save(net.state_dict(), model_dir + model_name+"_bce_itr_%d_train_%3f_tar_%3f.pth" %
                            (ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
                 running_loss = 0.0
